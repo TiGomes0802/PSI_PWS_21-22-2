f9a4bc6ae34b02f7be500ad888d3336c72977e36
TiGomes0802 <tigomes08@gmail.com>
2022-06-16 23:44:39 +0100

    update

--- PSI_PWS_21-22/views/auth/index.php
+++ PSI_PWS_21-22/views/auth/index.php
@@ -4,7 +4,9 @@
     <form class="form1" action="./router.php?c=auth&a=login" method="POST">
         <input class="un " type="text" align="center" placeholder="Username" name="username">
         <input class="pass" type="password" align="center" placeholder="Password" name="password">
-        <input class="submit" type="submit" value="Submit">
+        <div align="center">
+            <input class="submit" type="submit" value="login">
+        </div>
     </form>
 </div>
 
